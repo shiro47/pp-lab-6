f4a1667b289ce2bf1583ecf350620d7c777982a4
shiro_pc <przemek.fied2@gmail.com>
2024-05-05 11:35:12 +0200

    zad3

--- Main.java
+++ Main.java
@@ -7,28 +7,19 @@ import company.models.Worker;
 
 public class Main {
     public static void main(String[] args) {
-        List<Employee> employees = new ArrayList<>();
-
         Worker worker1 = new Worker("John", 2000, 1, "2022-01-01", "Junior Developer");
         Worker worker2 = new Worker("Alice", 1800, 2, "2022-03-15", "Intern");
-        Worker worker3 = new Worker("Bob", 2200, 3, "2021-11-20", "Senior Developer");
-        Worker worker4 = new Worker("Emma", 1900, 4, "2023-02-10", "Project Manager");
+        Worker worker3 = new Worker("Bob", 2200, 2, "2021-11-20", "Senior Developer");
 
         Manager manager = new Manager("Michael", 3500, 5, "2020-05-10", "CTO");
 
-        employees.add(worker1);
-        employees.add(worker2);
-        employees.add(worker3);
-        employees.add(worker4);
-        employees.add(manager);
+        System.out.println(worker1.getName() + " has code: " + worker1.hashCode());
+        System.out.println(worker2.getName() + " has code: " + worker2.hashCode());
+        System.out.println(worker3.getName() + " has code: " + worker3.hashCode());
+        System.out.println(manager.getName() + " has code: " + manager.hashCode());
 
-        for (Employee employee : employees) {
-            System.out.println("- " + employee.getName() +
-                    " (ID: " + employee.hashCode() +
-                    ", Position: " + employee.getPosition() +
-                    ", Hire date: " + employee.getHireDate() +
-                    ", Salary: " + employee.getSalary() + ")");
-            employee.work();
-        }
+        System.out.println(worker2.getName() + " equals worker1: " + worker2.equals(worker1));
+        System.out.println(worker2.getName() + " equals worker3: " + worker2.equals(worker3));
+        System.out.println(worker2.getName() + " equals manager: " + worker2.equals(manager));
     }
 }
--- company/abstracts/Employee.java
+++ company/abstracts/Employee.java
@@ -37,4 +37,16 @@ public abstract class Employee implements Employable {
     public int hashCode() {
         return id;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+
+        Employee other = (Employee) obj;
+        return id == other.id;
+    }
 }
